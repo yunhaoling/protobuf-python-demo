754d130110aec326f7bbe09205ae8a6934afb08a
Yunhao Ling <adam_ling@outlook.com>
2020-09-15 21:11:31 -0700

    dynamic type generation

--- demo/schema_demo.py
+++ demo/schema_demo.py
@@ -1,51 +1,63 @@
 import base64
 
-from google.protobuf import descriptor_pb2
-from google.protobuf import descriptor
+from google.protobuf import descriptor, descriptor_pb2, message_factory
 from google.protobuf.message_factory import MessageFactory
 from google.protobuf.descriptor_pool import DescriptorPool
 
 
 from generated_code.demo import addressbook_pb2
 
+
+# TODO: What's definition of the schema in a protobuf context
+#   - To locate a specific message type: it should be proto + class name
+
+
 def schema_to_str(proto_file):
     """
-    Base64 encodes a FileDescriptor
     Args:
         proto_file (FileDescriptor): FileDescriptor to encode.
     Returns:
-        str: Base64 encoded FileDescriptor
+        str: Encoded FileDescriptor
     """
     return proto_file.serialized_pb
-    #return base64.standard_b64encode(proto_file.serialized_pb).decode('ascii')
+    # TODO: confluent use base64 to encode a FileDescriptor
+    # return base64.standard_b64encode(proto_file.serialized_pb).decode('ascii')
+
 
 def str_to_schema(raw_schema):
     file_descriptor_proto = descriptor_pb2.FileDescriptorProto.FromString(raw_schema)
-    
+
     descriptor_pool = DescriptorPool()
     descriptor_pool.Add(file_descriptor_proto)
 
-    # message_descriptors = []
-    # for message_type in file_descriptor_proto.message_type:
-    #     print(message_type)
-    #     # The following line would raise error asking to import 
-    #     message_descriptors.append(descriptor.MakeDescriptor(message_type))
-    
     name = file_descriptor_proto.name
-    file_descriptor = descriptor_pool.FindFileByName(name)
-    descriptor1 = descriptor_pool.FindMessageTypeByName('tutorial.Person')
-    descriptor2 = descriptor_pool.FindMessageTypeByName('tutorial.AddressBook')
-    descriptor3 = descriptor_pool.FindMessageTypeByName('tutorial.Person.PhoneNumber')
+    descriptor_person = descriptor_pool.FindMessageTypeByName('tutorial.Person')
+    descriptor_addressbook = descriptor_pool.FindMessageTypeByName('tutorial.AddressBook')
+    descriptor_phonenumber = descriptor_pool.FindMessageTypeByName('tutorial.Person.PhoneNumber')
 
-    return descriptor1, descriptor2, descriptor3
+    person_class = MessageFactory().GetPrototype(descriptor_person)
+    addressbook_class = MessageFactory().GetPrototype(descriptor_addressbook)
+    phonenumber_class = MessageFactory().GetPrototype(descriptor_phonenumber)
+
+    # A different way to extract types from the proto
+    # messages_type_dic = message_factory.GetMessages([file_descriptor_proto])
+    # # messages_type_dic only contain 'tutorial.Person' and 'tutorial.AddressBook'
+    # assert len(messages_type_dic) == 2
+    # assert 'tutorial.Person' in messages_type_dic
+    # assert 'tutorial.AddressBook' in messages_type_dic
+    # person_class = messages_type_dic['tutorial.Person']
+    # addressbook_class = messages_type_dic['tutorial.AddressBook']
+    # person_instance = person_class()
+    # addressbook_instance = addressbook_class()
+    # return person_class, addressbook_class
+
+    return person_class, addressbook_class, phonenumber_class
 
 
 protobuf_class = addressbook_pb2.AddressBook
 
 schema_str = schema_to_str(protobuf_class.DESCRIPTOR.file)
-descriptor1, descriptor2, descriptor3 = str_to_schema(schema_str)
+person_class, addressbook_class, phonenumber_class = str_to_schema(schema_str)
 
-msg_class = MessageFactory().GetPrototype(descriptor1)
-
-msg = msg_class()
+msg = person_class()
 print(type(msg))
